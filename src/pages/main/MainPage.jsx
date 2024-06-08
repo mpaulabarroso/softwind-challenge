5c11c16ae322c978cdba46126efb820a8be20cbf
mpaulabarroso <mariapaulab90@gmail.com>
2024-06-08 00:29:24 +0000

    refactor: reaponsive card detail

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -17,7 +17,7 @@ export function MainPage() {
                                 image={img}
                                 price={'$ 50000'}
                                 address={'Congreso 1920'}
-                                description={'Cerca del centro y a cuadras del parque'}
+                                description={'Cerca del centro y a cuadras del parque. Acogedora casa adosada de dos dormitorios ubicada en un pintoresco vecindario residencial. Esta encantadora propiedad cuenta con una amplia sala de estar, una cocina moderna totalmente equipada y un hermoso patio trasero perfecto para entretenimiento al aire libre. Con una ubicación conveniente cerca de parques, escuelas y tiendas locales, esta propiedad es ideal para parejas jóvenes o familias que buscan establecerse en un entorno tranquilo y acogedor.'}
                             />
                         }
                         onPress={
@@ -25,7 +25,7 @@ export function MainPage() {
                                 image={img}
                                 price={'$ 50000'}
                                 address={'Congreso 1920'}
-                                description={'Cerca del centro y a cuadras del parque'}
+                                description={'Cerca del centro y a cuadras del parque...'}
                             />
                         }
                     />
--- src/pages/main/components/product-detail-card/ProductDetailCard.jsx
+++ src/pages/main/components/product-detail-card/ProductDetailCard.jsx
@@ -3,10 +3,15 @@ import styles from './productDetailCard.module.css'
 export function ProductDetailCard({ price, address, description, image }) {
     return (
         <div className={styles.container}>
-            <img src={image} alt={address} className={styles.image} />
+            <div className={styles.header}>
+                <img src={image} alt={address} className={styles.image} />
+                <div>
+                    <p className={styles.price}>{price}</p>
+                    <h3 className={styles.title}>{address}</h3>
+                </div>
+            </div>
             <div className={styles.content}>
-                <p className={styles.price}>{price}</p>
-                <h3 className={styles.title}>{address}</h3>
+                <h4 className={styles.subtitle}>Sobre esta propiedad</h4>
                 <p className={styles.description}>{description}</p>
             </div>
         </div>
